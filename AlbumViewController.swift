163452583d85e29ef1e24c2d7fd06b94b5beead3
Liam Burdett <liamburdett@me.com>
2017-10-23 13:10:49 +0100

    1st commit since changing blank document.

---
+++ AlbumViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AlbumViewController.swift
+//  VinylCollector
+//
+//  Created by Liam Burdett on 23/10/2017.
+//  Copyright © 2017 Outlier. All rights reserved.
+//
+
+import UIKit
+
+class AlbumViewController: UIViewController {
+
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+    @IBOutlet weak var artistTextField: UITextField!
+    
+    @IBOutlet weak var albumTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+   
+
+
+
+}
+    @IBAction func addTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+}
